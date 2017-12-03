f51a399d24d9da29abc834f1d840dc1b1b7deb1c
wayfajjjh <33476561+wayfajjjh@users.noreply.github.com>
2017-12-03 20:40:50 +0700

    Update login_pelamar.php

--- pelamar/login_pelamar.php
+++ pelamar/login_pelamar.php
@@ -9,7 +9,7 @@
                             <!--<img src="img/logo.png" width="90px" style="margin-bottom: 20px;"/>-->
                             <h7 class="login-text">Login sebagai <b>Calon Pekerja</b>.</h7>
                             <input class="form-control" type="text" name="username" placeholder="Username" required/>
-                            <input class="form-control" type="password" name="password" placeholder="Password" required/>
+                            <input class="form-control" type="password" name="pass" placeholder="Password" required/>
                             <input class="btn btn-primary" type="submit" value="Login" style="padding: 14px 20px; margin-top: 20px;"
                                 required/>
                         </form>
